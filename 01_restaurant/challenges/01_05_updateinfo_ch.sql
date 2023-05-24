d16fb43e71c943f8524d90e13abc8d880a28a392
BURGHARDT Andrea <andrea.burghardt@immowelt.de>
2023-05-24 15:08:32 +0000

    weitere Challenges

--- 01_restaurant/challenges/01_05_updateinfo_ch.sql
+++ 01_restaurant/challenges/01_05_updateinfo_ch.sql
@@ -2,3 +2,9 @@
 
 -- Taylor Jenkins aus 27170 6th Ave., Washington, DC,
 -- ist umgezogen nach 74 Pine St., New York, NY.
+
+SELECT * FROM Kunden where nachname like 'Jenkins'
+UPDATE Kunden
+SET Adresse = '74 Pine St.', Stadt = 'New York',
+ State = 'NY'
+WHERE KundenID = 26
